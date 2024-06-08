7c09cdedfff37c786fc76c43f3dced11bfb21456
Alex Hasha <ahasha@gmail.com>
2024-06-08 09:16:55 -0400

    Revising to log metrics and build longer contexts from multiple pages.

--- llm_learning/analyze.py
+++ llm_learning/analyze.py
@@ -7,12 +7,15 @@ from typing import Iterator, List, Optional
 
 import dotenv
 import pandas as pd
+import tiktoken
 import typer
 from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from pydantic import ValidationError
 from typing_extensions import Annotated
 
+from dvclive import Live
 from llm_learning.logging_config import configure_logging
 
 logger = logging.getLogger(__name__)
@@ -24,102 +27,89 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 app = typer.Typer()
 
+CONTEXT_WINDOW = {
+    "gpt-4o": 128000,
+    "gpt-3.5-turbo": 16000,
+}
 
-class EmissionsCategory(str, Enum):
+# cl100k_base is appropriate for
+# gpt-4, gpt-3.5-turbo, text-embedding-ada-002, text-embedding-3-small, text-embedding-3-large
+# Source https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+TOKENIZER = {
+    "gpt-4o": "o200k_base",
+    "gpt-3.5-turbo": "cl100k_base",
+    "gpt-4": "cl100k_base",
+}
+
+
+class ActionCategory(str, Enum):
+    Governance = "G"
+    Zoning = "Z"
     Buildings = "B"
     Energy = "E"
     Transportation = "T"
     Waste = "W"
-    LandUse = "L"
+    Conservation = "C"
 
 
 class Goal(BaseModel):
     """Information about a strategic planning Goal.
 
-    Goals are broad, quantifiable outcomes necessary to meet emissions targets and resilience goals.
-    If the goal does not mention a quantitative target or a target year, you should classify it as a Strategy instead.
+    Goals are quantified outcomes necessary to meet emissions targets and resilience goals.
+    If the goal does not mention a quantitative target and a target year, you should skip it or classify it as an Action instead.
     """
 
-    id: str = Field(
-        description="Unique Identifier.  If a Goal ID is given in the text, use this.  Otherwise, the first letter should be emissions_category letter."
+    document_goal_id: Optional[str] = Field(
+        description="If a unique identifier for the Goal ID is given in the text record it here."
     )
-    emissions_category: EmissionsCategory = Field(
+    action_category: ActionCategory = Field(
         description="""
-        The category of emissions the goal is associated with, e.g. Buildings, Transportation, Waste or Land Use, Energy.
-        Select Energy only if no other more specific emission category is mentioned, or if the goal pertains to electricity.
+        The Action Category the goal is associated with, e.g. Buildings, Transportation, Waste, Governance, Conservation, or Energy.
+        Select Energy only if no other more specific emission category is mentioned, or if the goal pertains specifically to electricity.
+        Governance is for goals related to municipal staffing, policies, or processes to support execution of the Climate Action Plan.
         """
     )
     year: Optional[int] = Field(
         default=None,
         description="The year by which the goal should be achieved.",
         ge=2024,
-        le=2050,
+        le=2100,
     )
     description: str = Field(
-        description="A summary description of the goal which must include a measurable, quantitative outcome",
+        description="A summary description of the goal, which must include a quantitative target and a target year.",
     )
     context: str = Field(
         description="Verbatim text from the provided document on which the Goal description is based"
     )
-
-
-class Strategy(BaseModel):
-    """Information about a strategic planning Strategy.
-
-    Strategies define general approaches to make progress toward goals.
-    They should be specific, but need not necessarily be quantifiable.
-    They are frequently explicitly labeled as a "Strategy" in the text.
-    However, they are often described in the text as "Goals", but with no associated quantitative outcome mentioned.
-    """
-
-    id: str = Field(
-        description="Unique Identifier.  If a Strategy ID is given in the text, use this.  Otherwise, the first letter should be emissions_category letter."
-    )
-    emissions_category: EmissionsCategory = Field(
-        description="""
-        The category of emissions the goal is associated with, e.g. Buildings, Transportation, Waste or Land Use, Energy.
-        Select Energy only if no other more specific emission category is mentioned, or if the goal pertains to electricity.
-        """
-    )
-    related_goals: Optional[List[str]] = Field(
-        description="A list of goal ids that this strategy is related to"
-    )
-    description: str = Field(description="A description of the strategy")
-    context: str = Field(
-        description="Verbatim text from the provided document on which the Strategy description is based"
+    context_page: int = Field(
+        description="The page number of the document that the context string was drawn from."
     )
 
 
 class Action(BaseModel):
-    """Information about a strategic planning Action.
-
-    Actions are specific, time-bound steps to implement strategies.
-    If the Action does not mention an owner or is very general, you should classify it as a Strategy instead.
-    """
+    """Information about an action item described in the document."""
 
     id: str = Field(
-        description="Unique Identifier.  First category should be emissions_category letter."
+        description="If a Unique Identifier for the action is given in the text, reecord it here."
     )
-    emissions_category: EmissionsCategory = Field(
+    emissions_category: ActionCategory = Field(
         description="The category of emissions the strategy is associated with"
     )
     owner: Optional[List[str]] = Field(
         default=None,
-        description="The organization or individuals responsible for the action, if known",
-    )
-    related_stragegies: Optional[List[str]] = Field(
-        default=None,
-        description="A list of strategy ids that this action is related to",
+        description="The entities or individuals responsible for the action, if mentioned in the text",
     )
     description: str = Field(description="A summary description of the action")
     context: str = Field(
         description="Verbatim text from the provided document on which the Action description is based"
     )
+    context_page: int = Field(
+        description="The page number of the document that the context string was drawn from."
+    )
 
 
 class Results(BaseModel):
     goals: Optional[List[Goal]] = []
-    strategies: Optional[List[Strategy]] = []
     actions: Optional[List[Action]] = []
 
 
@@ -128,23 +118,9 @@ def results_to_goals_table(results, page, municipality):
         {
             **g.dict(),
             "document_url": page.metadata["source"],
-            "page": page.metadata["page"],
             "municipality": municipality,
         }
         for g in results.goals
-    ]
-    return pd.DataFrame(records)
-
-
-def results_to_strategies_table(results, page, municipality):
-    records = [
-        {
-            **s.dict(),
-            "document_url": page.metadata["source"],
-            "page": page.metadata["page"],
-            "municipality": municipality,
-        }
-        for s in results.strategies
     ]
     return pd.DataFrame(records)
 
@@ -154,7 +130,6 @@ def results_to_actions_table(results, page, municipality):
         {
             **a.dict(),
             "document_url": page.metadata["source"],
-            "page": page.metadata["page"],
             "municipality": municipality,
         }
         for a in results.actions
@@ -162,18 +137,65 @@ def results_to_actions_table(results, page, municipality):
     return pd.DataFrame(records)
 
 
-def chunk(pdf: Path) -> Iterator[Document]:
+# Function to count tokens in a document
+def count_tokens(text, tokenizer):
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+
+def document_iterator(pdf: Path, max_tokens: int, model: str) -> Iterator[Document]:
+    # Initialize the tokenizer.
+    encoding = TOKENIZER[model]
+    tokenizer = tiktoken.get_encoding(encoding)
+
     from langchain_community.document_loaders import PyMuPDFLoader
 
     loader = PyMuPDFLoader(pdf)
     pages = loader.load()
+    full_document_content = ""
+    first_page = 0
+    last_page = 0
+    chunk_tokens = 0
     for page in pages:
-        yield page
+        page_number = int(page.metadata["page"])
+        new_content = f"PAGE {page_number}\n\n{page.page_content}"
+        _full_doc_content = full_document_content + "\n\n" + new_content
+        expanded_chunk_tokens = count_tokens(_full_doc_content, tokenizer)
+        if expanded_chunk_tokens > max_tokens:
+            # Adding this page would take you over the token limit, so yield the current chunk.
+            document = Document(
+                full_document_content,
+                metadata={
+                    "first_page": first_page,
+                    "last_page": last_page,
+                    "source": str(pdf),
+                    "chunk_tokens": chunk_tokens,
+                },
+            )
+            yield document
+            # Reset the working chunk to only the current page.
+            first_page = page_number
+            last_page = page_number
+            full_document_content = new_content
+            chunk_tokens = count_tokens(new_content, tokenizer)
+        else:  # chunk is still under the token limit, add onto it.
+            chunk_tokens = expanded_chunk_tokens
+            last_page = page_number
+            full_document_content = _full_doc_content
+
+    document = Document(
+        full_document_content,
+        metadata={
+            "first_page": first_page,
+            "last_page": last_page,
+            "source": str(pdf),
+            "chunk_tokens": chunk_tokens,
+        },
+    )
+    yield document
 
 
-def get_prompt():
-    from langchain_core.prompts import ChatPromptTemplate
-
+def get_prompt_template() -> ChatPromptTemplate:
     # Define a custom prompt to provide instructions and any additional context.
     # 1) You can add examples into the prompt template to improve extraction quality
     # 2) Introduce additional parameters to take context into account (e.g., include metadata
@@ -184,13 +206,13 @@ def get_prompt():
                 "system",
                 "You are an expert municipal climate action planner "
                 """Read the provided Climate Action Plan document and extract the following information described in the text:
-                1. Emissions reductions Goals;
-                2. Strategies to achieve the goals;
-                3. Action items related to the Strategies; """
-                "If the text does not describe a goal, strategy, or action, do not make up an answer."
+                1. Goals;
+                2. Action items to achieve the goals; """
+                "If the text does not explicitly describe a goal or action, do not make up an answer."
                 "Only extract relevant information from the text."
-                "If the value of an attribute you are asked to extract is not present in the text, return null for the attribute's value."
-                "The same text cannot be both a goal and a strategy.  Please distinguish them based on their definitions.",
+                "However, be exhaustive, and extract all goals and actions that are explicitly mentioned in the text. "
+                "There will often be multiple goals and actions mentioned on each page. "
+                "If the value of an attribute you are asked to extract is not present in the text, return null for the attribute's value.",
             ),
             # Please see the how-to about improving performance with
             # reference examples.
@@ -202,24 +224,18 @@ def get_prompt():
 
 
 @app.command()
-def chunks(pdf: Path):
-    for page in chunk(pdf):
-        print(f"## Page {page.metadata['page']} from {page.metadata['source']}")
-        print(page.page_content)
-
-
-@app.command()
 def extract(
     pdf: Path,
     llm_model: Annotated[str, typer.Option()] = "gpt-3.5-turbo",
 ):
+    if llm_model not in CONTEXT_WINDOW.keys():
+        raise ValueError(f"Invalid LLM model: {llm_model}")
     if not pdf.suffix == ".pdf":
         ValueError(f"File {pdf} is not a PDF file.")
     if not pdf.exists():
         ValueError(f"PDF file {pdf} does not exist.")
 
     goals_path = Path("data/processed", pdf.stem + "-goals.csv")
-    strategies_path = Path("data/processed", pdf.stem + "-strategies.csv")
     actions_path = Path("data/processed", pdf.stem + "-actions.csv")
     json_path = Path("data/processed", pdf.stem + ".json")
     municipality = pdf.stem.replace("-", ", ")
@@ -229,65 +245,56 @@ def extract(
     from langchain_openai import ChatOpenAI
 
     llm = ChatOpenAI(model=llm_model, temperature=0)
-    prompt = get_prompt()
+    prompt = get_prompt_template()
     runnable = prompt | llm.with_structured_output(schema=Results)
 
     goals_tables = []
-    strategies_tables = []
     actions_tables = []
     json_output = []
-    for page in chunk(pdf):
+    tokens_processed = 0
+    for chunk in document_iterator(pdf, 0.8 * CONTEXT_WINDOW[llm_model], llm_model):
         logger.info(
-            f"Processing page {page.metadata['page']} from {page.metadata['source']}"
+            f"Processing pages {chunk.metadata['first_page']} to {chunk.metadata['last_page']} from {chunk.metadata['source']}"
         )
-        # Display page content to the user
-        print(page.page_content)
-        # Ask user whether to proceed with AI extraction
         try:
-            # user_input = input("Proceed with AI extraction? (y/n/q): ")
-            # if user_input.lower() == "q":
-            #     break
-            # elif user_input.lower() != "y":
-            #     continue
-
-            logger.info("Extracting from page %s...", page.metadata["page"])
             try:
-                result = runnable.invoke({"text": page.page_content})
+                result = runnable.invoke({"text": chunk.page_content})
             except ValidationError as e:
                 logger.error(f"A validation error occurred: {str(e)}")
                 continue
 
             json_output.append(
                 {
-                    "source": page.metadata["source"],
-                    "page": page.metadata["page"],
-                    "content": page.page_content,
+                    "source": chunk.metadata["source"],
+                    "first_page": chunk.metadata["first_page"],
+                    "last_page": chunk.metadata["last_page"],
+                    "chunk_tokens": chunk.metadata["chunk_tokens"],
+                    "content": chunk.page_content,
                     "municipality": municipality,
                     "results": result.dict(),
                 }
             )
-            json_output_str = json.dumps(json_output)
-            with open(json_path, "w") as f:
-                f.write(json_output_str)
+            tokens_processed += chunk.metadata["chunk_tokens"]
 
-            goals_tables.append(results_to_goals_table(result, page, municipality))
-            strategies_tables.append(
-                results_to_strategies_table(result, page, municipality)
-            )
-            actions_tables.append(results_to_actions_table(result, page, municipality))
+            goals_tables.append(results_to_goals_table(result, chunk, municipality))
+            actions_tables.append(results_to_actions_table(result, chunk, municipality))
         except Exception as e:
             logger.error(f"An error occurred: {str(e)}")
 
     goals_df = pd.concat(goals_tables)
-    strategies_df = pd.concat(strategies_tables)
     actions_df = pd.concat(actions_tables)
 
+    with Live() as live:
+        live.log_metric(f"{municipality}/goals_extracted", goals_df.shape[0])
+        live.log_metric(f"{municipality}/actions_extracted", actions_df.shape[0])
+        live.log_metric(f"{municipality}/doc_tokens_processed", tokens_processed)
     logger.info("Writing goals to %s", goals_path)
     goals_df.to_csv(goals_path, index=False)
-    logger.info("Writing strategies to %s", strategies_path)
-    strategies_df.to_csv(strategies_path, index=False)
     logger.info("Writing actions to %s", actions_path)
     actions_df.to_csv(actions_path, index=False)
+
+    with open(json_path, "w") as f:
+        json.dump(json_output, f, indent=2)
 
 
 if __name__ == "__main__":
