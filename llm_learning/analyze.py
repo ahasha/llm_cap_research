200ebd6e6e286c4e8b317b38f103a6a58b5f4deb
Alex Hasha <ahasha@gmail.com>
2024-06-08 12:14:26 -0400

    Longer context input.  DOESNT WORK
    This approach is hitting the output context token limit for gpt-4o, which is only 4,096 tokens.  Gpt-4 and
    3.5-turbo are also limited to 4,096.  Trying to put the whole table of goals and actions in the same output will hit this pretty quickly.

--- llm_learning/analyze.py
+++ llm_learning/analyze.py
@@ -223,6 +223,54 @@ def get_prompt_template() -> ChatPromptTemplate:
     return prompt
 
 
+def check_output(tool_output):
+    """Check for parse error or failure to call the tool"""
+
+    # Error with parsing
+    if tool_output["parsing_error"]:
+        # Report back output and parsing errors
+        print("Parsing error!")
+        raw_output = str(tool_output["raw"].content)
+        error = tool_output["parsing_error"]
+        raise ValueError(
+            f"Error parsing your output! Be sure to invoke the tool. Output: {raw_output}. \n Parse error: {error}"
+        )
+
+    # Tool was not invoked
+    elif not tool_output["parsed"]:
+        print("Failed to invoke tool!")
+        raise ValueError(
+            "You did not use the provided tool! Be sure to invoke the tool to structure the output."
+        )
+    return tool_output
+
+
+def insert_errors(inputs):
+    breakpoint()
+    """Insert errors for tool parsing in the messages"""
+
+    # Get errors
+    error = inputs["error"]
+    messages = inputs["messages"]
+    messages += [
+        (
+            "assistant",
+            f"Retry. You are required to fix the parsing errors: {error} \n\n You must invoke the provided tool.",
+        )
+    ]
+    return {
+        "messages": messages,
+        "context": inputs["context"],
+    }
+
+
+def parse_output(solution):
+    """When we add 'include_raw=True' to structured output,
+    it will return a dict w 'raw', 'parsed', 'parsing_error'."""
+
+    return solution["parsed"]
+
+
 @app.command()
 def extract(
     pdf: Path,
@@ -246,7 +294,18 @@ def extract(
 
     llm = ChatOpenAI(model=llm_model, temperature=0)
     prompt = get_prompt_template()
-    runnable = prompt | llm.with_structured_output(schema=Results)
+    runnable_raw = (
+        prompt
+        | llm.with_structured_output(schema=Results, include_raw=True)
+        | check_output
+    )
+    fallback_chain = prompt | insert_errors | runnable_raw
+    N = 3  # Max re-tries
+    runnable_with_retry = runnable_raw.with_fallbacks(
+        fallbacks=[fallback_chain] * N, exception_key="error"
+    )
+
+    chain = runnable_with_retry | parse_output
 
     goals_tables = []
     actions_tables = []
@@ -258,7 +317,7 @@ def extract(
         )
         try:
             try:
-                result = runnable.invoke({"text": chunk.page_content})
+                result = chain.invoke({"text": chunk.page_content})
             except ValidationError as e:
                 logger.error(f"A validation error occurred: {str(e)}")
                 continue
