13f38b210026b11d1a072ec3942bb21552857c5b
Alex Hasha <ahasha@gmail.com>
2024-06-29 15:29:33 -0400

    Documentation formatting updates

--- docs/conf.py
+++ docs/conf.py
@@ -161,8 +161,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-# html_logo = "your_logo.png"
-# html_favicon = "favicon.ico"
+html_logo = "color_logo_with_background.png"
+html_favicon = "favicon.ico"
 # html_scaled_image_link = False
 
 # If not "", a "Last updated on:" timestamp is inserted at every page bottom,
